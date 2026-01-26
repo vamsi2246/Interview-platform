946aa8d298a73506426a706a47060fcabc724635
Vamsi <kummaravamsi2246@gmail.com>
2026-01-26 23:31:41 +0530

    added login and logout buttton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-// import {SignInButton} from '@clerk/clerk-react'
+import {SignedIn, SignedOut, SignInButton, SignOutButton, UserButton} from '@clerk/clerk-react'
 import './App.css'
 
 function App() {
@@ -8,7 +8,16 @@ function App() {
   return (
     <>
       <h1>Welocome to our App</h1>
-      
+    
+      <SignedIn>
+        <SignInButton />
+      </SignedIn>
+
+      <SignedOut>
+        <SignInButton/>
+      </SignedOut>
+
+      <UserButton/>
     </>
   )
 }
