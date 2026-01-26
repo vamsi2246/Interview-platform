cf23f50b04cf3b50b6663e87906536de7572a848
vamsi2246 <vamsi2246@users.noreply.github.com>
2026-01-26 13:43:19 +0530

    initial commit

---
+++ Backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.status(200).json({msg:"success from api"})
+})
+app.listen(ENV.PORT,()=>{
+    console.log("server running on port:",ENV.PORT)
+})
