15ad27dba84cf520e6bd64a26f476d7225930f17
Vamsi <kummaravamsi2246@gmail.com>
2026-01-26 19:46:55 +0530

    database connected

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,22 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { connect } from "mongoose";
 const app=express()
 
 app.get("/",(req,res)=>{
     res.status(200).json({msg:"success from api"})
 })
-app.listen(ENV.PORT,()=>{
-    console.log("server running on port:",ENV.PORT)
+
+const startServer= async ()=>{
+    try{
+        await connectDB()
+        app.listen(ENV.PORT,()=>{
+        console.log("server running on port:",ENV.PORT)
 })
+    }catch(error){
+        console.error("Error in starting the server")
+    }
+    
+}
+startServer()
