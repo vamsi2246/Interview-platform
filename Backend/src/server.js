18979fb3316c0e33fbe8943f5bd841a1464c6d6f
vamsi2246 <vamsi2246@users.noreply.github.com>
2026-01-30 21:33:21 +0530

    added api

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,7 +2,11 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { connect } from "mongoose";
+import cors from 'cors'
 const app=express()
+
+app.use(express.json())
+app.use(cors({origin:ENV.ClIENT_URL,Credentials:true}))
 
 app.get("/",(req,res)=>{
     res.status(200).json({msg:"success from api"})
