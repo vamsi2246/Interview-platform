ade11d32db93673f4db8ccb3f3a4be7704030e43
Vamsi <kummaravamsi2246@gmail.com>
2026-01-30 21:33:21 +0530

    added api

--- Backend/src/lib/env.js
+++ Backend/src/lib/env.js
@@ -4,5 +4,12 @@ dotenv.config({quiet:true})
 
 export const ENV={
     PORT:process.env.PORT,
-    DB_URL:process.env.DB_URL
+    DB_URL:process.env.DB_URL,
+    INNGEST_EVEVT_KEY:process.env.INNGEST_EVEVT_KEY,
+    INNGEST_SIGNIN_KEY:process.env.INNGEST_SIGNIN_KEY,
+    STREAM_API_KEY:process.env.STREAM_API_KEY,
+    STREAM_API_SECRET:process.env.STREAM_API_SECRET,
+    CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
+    CLIENT_URL:process.env.CLIENT_URL
 }
