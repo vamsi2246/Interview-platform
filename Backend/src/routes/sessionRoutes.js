6f487f0ab605c3a09fb855540a7c2ae4209f46f8
vamsi2246 <vamsi2246@users.noreply.github.com>
2026-02-01 03:21:46 +0530

    added session Routes

---
+++ Backend/src/routes/sessionRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { protectRoute } from '../middlewares/protectRoute.js';
+const router=express.Router();
+
+
+router.post("/",protectRoute,createSession);
+
+router.get("/active",protectRoute,getActiveSessions);
+router.get("/my-recent",protectRoute,getMyRecentSessions);
+router.get("/:id",protectRoute,getSessionById);
+
+router.post("/:id/join",protectRoute,joinSession);
+router.post("/:id/end",protectRoute,endSession);
+
+export default router;
