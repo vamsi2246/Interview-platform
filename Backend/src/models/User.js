5193c6e297557b33eeeb0620b34acb1c26daabc8
Vamsi <kummaravamsi2246@gmail.com>
2026-02-01 19:47:10 +0530

    session controllers added

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -24,5 +24,5 @@ const userSchema=new mongoose.Schema(
     {timestamps:true}
 )
 
-const User=mongoose.model("User",userSchema)
+const User=mongoose.model("Use",userSchema)
 export default User;
--- Backend/src/routes/sessionRoutes.js
+++ Backend/src/routes/sessionRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { protectRoute } from '../middlewares/protectRoute.js';
+import {createSession,getActiveSessions,getMyRecentSessions,getSessionById,joinSession,endSession} from '../controllers/sessionController.js'
+
 const router=express.Router();
 
 
