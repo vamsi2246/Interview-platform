c1a48b4531ef3f418e98f820094c5a79338a77f9
Vamsi <kummaravamsi2246@gmail.com>
2026-02-03 01:03:07 +0530

    tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import {SignedIn, SignedOut, SignInButton, SignOutButton, UserButton} from '@clerk/clerk-react'
-import './App.css'
+import HomePage from './pages/HomePage.jsx';
+import { Routes, Route } from 'react-router';
+import AboutPage from './pages/AboutPage.jsx';  
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Welocome to our App</h1>
-    
-      <SignedIn>
+    <Routes>
+      <h1 className='text-red-500'>Welocome to our App</h1>
+      <button className='btn btn-secondary'>Click</button>
+      {/* <SignedIn>
         <SignInButton />
       </SignedIn>
 
@@ -17,8 +19,13 @@ function App() {
         <SignInButton/>
       </SignedOut>
 
-      <UserButton/>
-    </>
+      <UserButton/> */}
+
+      <Routes>
+        <Route path="/" element={<h1 className='text-red-500'>Welcome to our App</h1>} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+      </Routes>
+    </Routes>
   )
 }
 
