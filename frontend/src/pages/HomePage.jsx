609a1ecf666a277bee77b601498391c5e1f5d5b7
Vamsi <kummaravamsi2246@gmail.com>
2026-02-05 22:40:34 +0530

    axios vite api key

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 import { Toaster  } from 'react-hot-toast'
 
 const HomePage = () => {
+                                                 
   return (
     <>
-    
+      
       <button className='btn btn-secondary' onClick={()=>toast.success("This is a success toast")}>Click Me</button>
       
       <SignedOut>
