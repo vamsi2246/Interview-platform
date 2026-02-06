17a2f82a05e3ae062f74b895ca46e4928fd71303
vamsi2246 <vamsi2246@users.noreply.github.com>
2026-02-07 00:09:36 +0530

    sparkel and stsrted button

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,6 @@
 import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react'
+import { Link } from 'react-router-dom';
+import { Sparkles as SparklesIcon, ArrowRight as ArrowRightIcon } from 'lucide-react';
 import React from 'react'
 import { toast } from 'react-hot-toast'
 
@@ -6,7 +8,7 @@ const HomePage = () => {
 
   return (
     <>
-    <nav className="bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg">
+      <nav className="bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg">
         <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
           {/* LOGO */}
           <Link
