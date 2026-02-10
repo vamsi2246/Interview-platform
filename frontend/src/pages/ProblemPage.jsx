a1b29427e96de010aa5dbe37b4d752b4bd422006
vamsi2246 <vamsi2246@users.noreply.github.com>
2026-02-10 23:06:21 +0530

    problem page

---
+++ frontend/src/pages/ProblemPage.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { PROBLEMS } from "../data/problems";
+import Navbar from "../components/Navbar";
+
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import ProblemDescription from "../components/ProblemDescription";
+import OutputPanel from "../components/OutputPanel";
+import CodeEditorPanel from "../components/CodeEditorPanel";
+import { executeCode } from "../lib/piston";
+
+function ProblemPage() {
+     const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [currentProblemId, setCurrentProblemId] = useState("two-sum");
+  const [selectedLanguage, setSelectedLanguage] = useState("javascript");
+  const [code, setCode] = useState(PROBLEMS[currentProblemId].starterCode.javascript);
+  const [output, setOutput] = useState(null);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const currentProblem = PROBLEMS[currentProblemId];
+
+  
+  useEffect(() => {
+    if (id && PROBLEMS[id]) {
+      setCurrentProblemId(id);
+      setCode(PROBLEMS[id].starterCode[selectedLanguage]);
+      setOutput(null);
+    }
+  }, [id, selectedLanguage]);
+
+
+
+
+  
+}
