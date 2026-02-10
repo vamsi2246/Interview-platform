03ce764b2a692c881dcc7b65e6185f7d2b7292ba
Vamsi <kummaravamsi2246@gmail.com>
2026-02-10 23:06:21 +0530

    problem page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
         <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
       </Routes>
 
       <Toaster position="top-right" />
